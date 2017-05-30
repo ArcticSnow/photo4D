b30644a221ccdb8441f7899a4c46ffdb612bbfca
arcticsnow <sfihol@alaska.edu>
2017-05-30 21:23:13 +0200

    added methodology

--- python_script/snow_segmentation.py
+++ python_script/snow_segmentation.py
@@ -4,10 +4,12 @@ import os, sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import skimage
+import skimage as sk
 
 '''
 Find best way to segment snow from rocks.
+
+First test using the JPEG image
 
 Export two images from raw:
     1 with good exposition for whites
@@ -16,8 +18,26 @@ Export two images from raw:
 Once segmented, equalize histogram, and combine the two segmented images
 '''
 
-# Include class and function here
+#===================================
+# Load JPEG image
 
+im_file = '/home/arcticsnow/Github/photo4D/python_script/image_sample/DSC00007.JPG'
+im = plt.imread(im_file)
+
+#===================================
+# Methods to test and assess which is best in diverse lighting situations
+
+#-----------------------------------
+# Method 1: Simple thresholding
+
+
+#-----------------------------------
+# Method 2: Chan-Vese Segmentation
+# http://scikit-image.org/docs/dev/auto_examples/segmentation/plot_chan_vese.html#sphx-glr-auto-examples-segmentation-plot-chan-vese-py
+
+#-----------------------------------
+# Method 3: Random walker segmentation
+# http://scikit-image.org/docs/dev/auto_examples/segmentation/plot_random_walker_segmentation.html#sphx-glr-auto-examples-segmentation-plot-random-walker-segmentation-py
 
 
 # Include script in this if statement
