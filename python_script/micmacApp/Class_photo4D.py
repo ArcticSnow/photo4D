bfa5ce2a66302d7024b7035cf80d579f61d61ee7
Alexis <aperret2010@hotmail.fr>
2018-08-16 15:32:11 +0200

    first part of debugg

--- python_script/micmacApp/Class_photo4D.py
+++ python_script/micmacApp/Class_photo4D.py
@@ -5,191 +5,281 @@ Program XXX Part I
 
 '''
 
-
 # import all function
 
 # import public library
 import os
-import os.path.join as opj
+from os.path import join as opj
 import numpy as np
+from typing import Union
 from shutil import copyfile, rmtree, copytree
 
 # Import project libary
 import Process as proc
 import Utils as utils
-import DetectSift as ds
+import Detect_Sift as ds
 import XML_utils
-
-class photo4d(object):
-	
-	def __init__(self,project_path):
-		self.project_path = os.path.abspath(project_path)
-		self.picture_set_def = opj(self.project_path,'set_definition.txt')
-		self.cam_folders = [opj(self.project_path, cam) for cam in os.listdir(self.project_path + 'Image')]
-		self.result_folder = opj(self.project_path, 'Results')
-		self.selected_picture_set = -1
-
-		self.GCP_coords_file = None
-		self.GCP_xml = 'GCPs_pick'
-
-		self.ext = "jpg"
-		self.cond = None
+import Image_utils as iu
 
 
-	def sort_picture(self, timeInterval=600):
-		self.sorted_pictures = proc.sort_pictures(self.cam_folders, self.picture_set_def, timeInterval=timeInterval,ext=self.ext)
-		return self.sorted_pictures
+class Photo4d(object):
+    IMAGE_FOLDER = "Images"
+    MASK_FOLDER = "Masks"
+    GCP_FOLDER = "GCP"
+    GCP_xml = 'GCPs_pick'
+
+    def __init__(self, project_path):
+        if not os.path.exists(project_path):
+            print("ERROR The path " + project_path + " doesn't exists")
+            exit(1)
+
+        # add main folder
+        self.project_path = os.path.abspath(project_path)
+        print("Creation of object photo4d on the folder " + self.project_path)
+
+        # add camera folders
+        if os.path.exists(opj(self.project_path, Photo4d.IMAGE_FOLDER)):
+            self.cam_folders = [opj(self.project_path, Photo4d.IMAGE_FOLDER, cam) for cam in
+                                os.listdir(opj(self.project_path, Photo4d.IMAGE_FOLDER))]
+            self.nb_folders = len(self.cam_folders)
+            print("Added {} camera folders : \n  {}".format(self.nb_folders, '\n  '.join(self.cam_folders)))
+        else:
+            print('You must create a folder "' + Photo4d.IMAGE_FOLDER + '/" containing your camera folders')
+            exit(1)
+
+        # add picture sets
+        self.picture_set_def = opj(self.project_path, 'set_definition.txt')
+        if os.path.exists(self.picture_set_def):
+            self.sorted_pictures = utils.pictures_array_from_file(self.picture_set_def)
+            print("Added picture sets from " + self.picture_set_def)
+        else:
+            self.sorted_pictures = None
+        # set default selected set to the last one
+        self.selected_picture_set = -1
+
+        if os.path.exists(opj(self.project_path, Photo4d.MASK_FOLDER)):
+            self.masks = opj(self.project_path, Photo4d.MASK_FOLDER)
+            print("Masks created from ")
+        else:
+            self.masks = None
+
+        self.result_folder = opj(self.project_path, 'Results')
+
+        self.GCP_coords_file = None
+
+        self.ext = "jpg"
+
+        self.cond = None
+
+    def __str__(self):
+        string = "Project Photo4d located at " + self.project_path
+        string += "\n Contains {} camera folders : \n   {}".format(self.nb_folders, '\n   '.join(self.cam_folders))
+        if self.sorted_pictures is None:
+            string += "\n Pictures unsorted"
+        else:
+            string += "\n Pictures sorted in {} sets ".format(len(self.sorted_pictures))
+            string += "\n The current selected set is {}".format(self.sorted_pictures[self.selected_picture_set][1:])
+        return string
+
+    def sort_picture(self, time_interval=600):
+        self.sorted_pictures = proc.sort_pictures(self.cam_folders, self.picture_set_def, time_interval=time_interval,
+                                                  ext=self.ext)
+        return self.sorted_pictures
+
+    def check_picture(self, luminosity_thresh=1, blur_thresh=6):
+        if self.sorted_pictures is None:
+            print("ERROR You must launch the sort_pictures() method before check_pictures()")
+            exit(1)
+        self.sorted_pictures = proc.check_pictures(self.cam_folders, self.picture_set_def, self.sorted_pictures,
+                                                   lum_inf=luminosity_thresh,
+                                                   blur_inf=blur_thresh)
+        return self.sorted_pictures
+
+    def initial_orientation(self, resolution=5000, distortion_mode='Fraser', display=True, clahe=False,
+                            tileGridSize_clahe=8):
+        # add mkdir, and change dir
+        tmp_path = opj(self.project_path, "tmp")
+        if not os.path.exists(tmp_path): os.makedirs(tmp_path)
+        os.chdir(tmp_path)
+
+        # select the set of good pictures to estimate initial orientation
+        selected_line = self.sorted_pictures[self.selected_picture_set]
+        file_set = "("
+        for i in range(len(self.cam_folders)):
+            in_path = opj(self.cam_folders[i], selected_line[i + 1])
+            out_path = opj(tmp_path, selected_line[i + 1])
+            if clahe:
+                iu.process_clahe(in_path, tileGridSize_clahe, out_path=out_path)
+            else:
+                copyfile(in_path, out_path)
+            file_set += selected_line[i + 1] + "|"
+        file_set = file_set[:-1] + ")"
+        print(file_set)
+
+        # Execute mm3d command for orientation
+        success, error = utils.exec_mm3d("mm3d Tapioca All {} {}".format(file_set, resolution), display=display)
+        success, error = utils.exec_mm3d("mm3d Tapas {} {} Out=ini".format(distortion_mode, file_set), display=display)
+
+        if success == 0:
+            # copy orientaion file and delete tmp folder
+            copytree(opj(tmp_path, "Ori-ini"), opj(self.project_path, "Ori-ini"))
+        else:
+            print("ERROR Orientation failed\nerror : " + str(error))
+
+        os.chdir(self.project_path)
+        # todo je sais pas pk si micmac plante je peux pas supprimer le dossier, alors que ca marche sur un autre test
+        rmtree(tmp_path)
+
+    def create_mask(self, del_pictures=True):
+        # add mkdir, and change dir
+        mask_path = opj(self.project_path, Photo4d.MASK_FOLDER)
+        if not os.path.exists(mask_path): os.makedirs(mask_path)
+        # select the set of good pictures to estimate initial orientation
+        selected_line = self.sorted_pictures[self.selected_picture_set]
+        for i in range(len(self.cam_folders)):
+            in_path = opj(self.cam_folders[i], selected_line[i + 1])
+            out_path = opj(mask_path, selected_line[i + 1])
+            copyfile(in_path, out_path)
+            ds.exec_mm3d('mm3d SaisieMasqQT {}'.format(out_path))
+            if del_pictures:
+                os.remove(out_path)
+        self.masks = mask_path
+
+    def prepare_GCP_files(self, GCP_coords_file, file_format='N_X_Y_Z', display=True):
+
+        if not os.path.exists(opj(self.project_path, Photo4d.GCP_FOLDER)):
+            os.makedirs(opj(self.project_path, Photo4d.GCP_FOLDER))
+
+        self.GCP_coords_file = opj(self.project_path, Photo4d.GCP_FOLDER, os.path.basename(GCP_coords_file))
+        copyfile(GCP_coords_file ,self.GCP_coords_file)
+
+        success, error = utils.exec_mm3d('mm3d GCPConvert #F={} {}'.format(file_format, self.GCP_coords_file),
+                                         display=display)
 
 
-	def check_picture(self, luminosity_thresh=1, blur_thresh=5):
-		self.sorted_pictures = check_picture(self.cam_folders, self.picture_set_def, lum_inf=luminosity_thresh, blur_inf=blur_thresh)
-		return self.sorted_pictures
+        GCP_table = np.loadtxt(self.GCP_coords_file, dtype=str, delimiter=' ')
 
 
-	def orientation_initial(self, resolution=5000, distortion_mode='Fraser', initial_calibration=None, display=True):
+        try:
+            GCP_name = GCP_table[:, file_format.split('_').index("N")]
+            np.savetxt(opj(self.project_path, "GCPs_name.txt"), GCP_name)
 
-		# select the set of good pictures to estimate initial orientation
-		selected_line = self.sorted_pictures[self.selected_picture_set]
-		file_set = "("
-		for i in range(len(cam_folders)):
-			file_set += opj(cam_folders[i], selected_line[ i + 1]) + "|"
-		file_set = file_set[:-1] + ")"
+        except:
+            print("ERROR prepare_GCP_files(): Check file format and file delimiter. Must be a single space delimiter")
 
-		# add mkdir, and change dir
-		tmp_path = opj(self.project_path,"tmp")
-		os.makedirs(tmp_path)
-		os.chdir(tmp_path)
+    # Prepare
 
-		# Execute mm3d command for orientation
-		success, error = utils.exec_mm3d("mm3d Tapioca All {} {}".format(file_set, resolution), display=display)
-		success, error = utils.exec_mm3d("mm3d Tapas {} {} Out=ini".format(distortion_mode, file_set), display=display)
+    def pick_GCP(self):
 
-		os.chdir(self.project_path)
-		# copy orientaion file and delete tmp folder
-		copytree(opj(tmp_path, "Ori-ini"), opj(self.project_path,"Ori-ini"))
-		rmtree(tmp_path)
+        os.chdir(opj(self.project_path, "GCP"))
 
-	def create_mask(self):
-		# add mkdir, and change dir
-		masq_path = opj(self.project_path,"Masks")
-		os.makedirs(masq_path)
-		os.chdir(mask_path)
-		# select the set of good pictures to estimate initial orientation
-		selected_line = self.sorted_pictures[self.selected_picture_set]
-		for i in range(len(cam_folders)):
-			ds.exec_mm3d('mm3d SaisieMasqQT {}'.format(opj(cam_folders[i], selected_line[ i + 1])))
-		os.chdir(self.project_path)
-		
+        # select the set of image on which to pick GCPs manually
+        selected_line = self.sorted_pictures[self.selected_picture_set]
+        file_set = "("
+        for i in range(len(self.cam_folders)):
+            file_set += opj(self.cam_folders[i], selected_line[i + 1]) + "|"
+        file_set = file_set[:-1] + ")"
 
-	def prepare_GCP_files(self, GCP_coords_file, file_format='N_X_Y_Z' display=True):
+        gcp_name_file = opj(self.project_path, "GCPs_name.txt")
+        utils.exec_mm3d("mm3d SaisieAppuisInitQt {} NONE {} {}.xml".format(file_set, gcp_name_file, self.GCP_xml))
 
-		os.makedirs(opj(self.project_path,"GCP"))
-		os.chdir(opj(self.project_path,"GCP"))
-		self.GCP_coords_file = opj(self.project_path, GCP_coords_file)
+        for folder in ['Tmp-SaisieAppuis', 'Tmp-MM-Dir']:
+            rmtree(folder)
+        os.remove(self.GCP_xml + '-S3D.xml')
+        os.chdir(self.project_path)
 
-		success, error = utils.exec_mm3d('mm3d GCPCoonvert "#F{}" {}'.format(file_format, self.GCP_coords_file), display=display)
-		
-		try:
-			GCP_table = np.loadtxt(self.GCP_coords_file, delimiter=' ')
-			GCP_name = GCP_table[:,file_format.split('_').index("N")]
-			np.savetxt(opj(self.project_path,"GCPs_name.txt"),GCP_name)
+    def detect_GCPs(self, kernel_size=(200, 200), display=True):
 
-		except:
-			print("ERROR prepare_GCP_files(): Check file format and file delimiter. Must be a single space delimiter")
+        xml_file = opj(self.project_path, 'GCP', self.GCP_xml + '-S2D.xml')
+        self.df_detect_gcp = ds.detect_from_s2d_xml(xml_file, self.cam_folders,
+                                                    self.sorted_pictures, kernel_size=kernel_size,
+                                                    display_micmac=display)
 
+    def extract_GCPs(self, magnitude_max=50, nb_values=5, max_dist=50, kernel_size=(200, 200), method="Median"):
 
-		os.chdir(self.project_path)
+        self.dict_image_gcp, self.df_gcp_abs = ds.extract_values(self.df_detect_gcp, magnitude_max=magnitude_max,
+                                                                 nb_values=nb_values, max_dist=max_dist,
+                                                                 kernel_size=kernel_size, method=method)
 
-		# Prepare 
+        # write xml file for the record. not necessary
+        XML_utils.write_S2D_xmlfile(self.dict_image_gcp, opj(self.project_path, 'GCP', 'GCPs_detect-S2D.xml'))
 
+    def process(self, clahe=False, tileGridSize_clahe=8, resol=-1, distortion_model="Fraser", re_estimate=True,
+                master_folder=0,
+                DefCor=0.0, shift=None, delete_temp=True, display=True):
 
-	def pick_GCP(self):
+        proc.process_from_array(self.cam_folders, self.sorted_pictures, self.result_folder,
+                                inori=opj(self.project_path, "Ori-ini"),
+                                gcp=self.GCP_coords_file, gcp_S2D=self.dict_image_gcp, clahe=clahe,
+                                tileGridSize_clahe=tileGridSize_clahe, resol=resol, distortion_model=distortion_model,
+                                re_estimate=re_estimate, master_folder=master_folder, masq2D=None, DefCor=DefCor,
+                                shift=shift, delete_temp=delete_temp,
+                                display_micmac=display, cond=self.cond)
 
-		os.chdir(opj(self.project_path,"GCP"))
+    def set_selected_set(self, img_or_index: Union[int, str]):
+        if self.sorted_pictures is None:
+            print("ERROR You must apply sort_pictures before trying to chose a set")
+            exit(1)
+        else:
+            if type(img_or_index) == int:
+                self.selected_picture_set = img_or_index
+                print(
+                    "\n The current selected set is now {}".format(self.sorted_pictures[self.selected_picture_set][1:]))
+            elif type(img_or_index) == str:
+                found, i = False, 0
+                while (not found) and (i < len(self.sorted_pictures)):
+                    if img_or_index in self.sorted_pictures[i]:
+                        found = True
+                        self.selected_picture_set = i
+                        print("\n The current selected set is now {}".format(
+                            self.sorted_pictures[self.selected_picture_set][1:]))
+                    i += 1
+                if not found:
+                    print('image {} not in sorted_pictures'.format(img_or_index))
 
-		# select the set of image on which to pick GCPs manually
-		selected_line = self.sorted_pictures[self.selected_picture_set]
-		file_set = "("
-		for i in range(len(cam_folders)):
-			file_set += opj(cam_folders[i], selected_line[ i + 1]) + "|"
-		file_set = file_set[:-1] + ")"
-
-		gcp_name_file = opj(self.project_path,"GCPs_name.txt")
-		utils.exec_mm3d("mm3d SaisieAppuisInitQt {} NONE {} {}.xml".format(file_set, gcp_name_file, self.GCP_xml))
-
-		for folder in ['Tmp-SaisieAppuis','Tmp-MM-Dir']:
-			rmtree(folder)
-		os.remove(self.GCP_xml + '-S3D.xml')
-		os.chdir(self.project_path)
-
-
-
-	def detect_GCPs(self, kernel_size=(200,200), display=True):
-
-		xml_file = opj(self.project_path, 'GCP', self.GCP_xml+'-S2D.xml')
-		self.df_detect_gcp = ds.detect_from_s2d_xml(xml_file, self.cam_folders, 
-				self.sorted_pictures, samples_folder=None, kernel_size=kernel_size, dipslay_micmac=display)
-
-
-	def extract_GCPs(self, magnitude_max=50, nb_values=5, max_dist=50, kernel_size=(200, 200), method="Median"):
-
-		self.dict_image_gcp, self.df_gcp_abs= ds.extract_values(self.df_detect_gcp, magnitude_max=magnitude_max, 
-			nb_values=nb_values, max_dist=max_dist, kernel_size=kernel_size, method=method)
-
-		# write xml file for the record. not necessary
-		XML_utils.write_S2D_xmlfile(self.dict_image_gcp, opj(self.project_path,'GCP','GCPs_detect-S2D.xml'))
-
-
-
-	def process(self, clahe=False, tileGridSize_clahe=8, resol=-1, distortion_model,re_estimate=True,master_folder=0,
-		DefCor=0.0,shift=None,delete_temp=True,display=True):
-
-		proc.process_from_array(self.cam_folders, self.sorted_pictures, self.result_folder, inori=opj(self.project_path,"Ori-ini"),
-			gcp=self.GCP_coords_file, gcp_S2D=self.dict_image_gcp,clahe=clahe, tileGridSize_clahe=tileGridSize_clahe, resol=resol,distortion_model,
-			re_estimate=re_estimate, master_folder=master_folder, masq2D=None, DefCor=DefCor, shift=shift, delete_temp=delete_temp,
-			display_micmac=display, cond=self.cond):
-
-
-	def add_cond(self):
-		# function to only execute program on a subseltection of images
-
-
-
-
+    def add_cond(self, condition):
+        if type(condition) != function:
+            print("WARNING input condition must be a function, which return a boolean and take a date as parameter")
+        else:
+            print("Condition applied")
+    # function to only execute program on a subseltection of images
 
 
 if __name__ == "__main__":
-	myproj = photo4d(project_path = "mypath/")
+    myproj = Photo4d(project_path="C:/Users/Alexis/Documents/Travail/Stage_Oslo/Grandeurnature/Pictures/Mini_projet")
+    # myproj.sort_picture()
+    # myproj.check_picture()
+    myproj.set_selected_set("DSC00857.JPG")
+    print(myproj)
+    # myproj.initial_orientation()
+    # myproj.create_mask()
+    myproj.prepare_GCP_files("C:/Users/Alexis/Documents/Travail/Stage_Oslo/Grandeurnature/GCP/Pt_gps_gcp.app")
+    # Part 1, sort and flag good picture set
+    # myproj.sort_picture()
+    # myproj.check_picture()
 
-	# Part 1, sort and flag good picture set
-	myproj.sort_picture()
-	myproj.check_picture()
+    # Part 2: Manual
+    # choose either:
+    # 		- Calibration, calibration of camera (TODO)
+    # 		- Orientation, estimate camera position
 
-	# Part 2: Manual
-		# choose either:
-		# 		- Calibration, calibration of camera (TODO)
-		# 		- Orientation, estimate camera position 
+    # myproj.selected_picture_set = -1  # by default get the last set of image. Change to the correct index where there are good quality images
+    # myproj.oriententation_intitial()
 
-	myproj.selected_picture_set = -1   # by default get the last set of image. Change to the correct index where there are good quality images
-	myproj.oriententation_intitial()
+    # myproj.selected_picture_set = -1
+    # myproj.create_mask()
 
-	myproj.selected_picture_set = -1
-	myproj.create_mask()
+    # Part 3: Derive GCPS in image stack
 
+    # myproj.prepare_GCP_files("GCPs.txt")  # format the text file to
 
-	# Part 3: Derive GCPS in image stack
-	
-	myproj.prepare_GCP_files("GCPs.txt")   # format the text file to 
-	
-	myproj.selected_picture_set = -1
-	myproj.pick_GCP()
+    # myproj.selected_picture_set = -1
+    # myproj.pick_GCP()
 
-	myproj.detect_GCPs()
+    # myproj.detect_GCPs()
 
+    # point GCP (manual)
+    # estimate on stack
 
-		# point GCP (manual)
-		# estimate on stack
-
-
-	# Part 4: Process images to point cloud
-	myproj.process()
+    # Part 4: Process images to point cloud
+    # myproj.process()
