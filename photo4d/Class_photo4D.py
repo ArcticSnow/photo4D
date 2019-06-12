a18019e271184621c26aa2de89c0a58f1aa71334
Luc Girod <lucg@uio.no>
2019-06-12 14:56:18 +0200

    Changed logic for GCP input

--- photo4d/Class_photo4D.py
+++ photo4d/Class_photo4D.py
@@ -39,11 +39,8 @@ class Photo4d(object):
     SET_FILE = 'set_definition.txt'
     GCP_PRECISION=0.2 # GCP precision in m
     GCP_POINTING_PRECISION=10 # Pointing precision of GCPs in images (pixels)
-    GCP_PICK_FILE_INI = 'GCPs_pick_Ini.xml'
-    GCP_PICK_FILE_INI_2D = 'GCPs_pick_Ini-S2D.xml'
-    GCP_PICK_FILE_BASC = 'GCPs_pick_Basc.xml'
-    GCP_PICK_FILE_BASC_2D = 'GCPs_pick_Basc-S2D.xml'
     GCP_PICK_FILE = 'GCPs_pick.xml'
+    GCP_PICK_FILE_2D = 'GCPs_pick-S2D.xml'
     GCP_DETECT_FILE = 'GCPs_detect-S2D.xml'
     GCP_NAME_FILE = 'GCPs_names.txt'
     shift=[410000, 6710000, 0]
@@ -51,7 +48,7 @@ class Photo4d(object):
     distortion_model="Figee"
     
     
-    def __init__(self, project_path, ext='jpg'):
+    def __init__(self, project_path, ext='JPG'):
         if not os.path.exists(project_path):
             print("ERROR The path " + project_path + " doesn't exists")
             return
@@ -127,6 +124,13 @@ class Photo4d(object):
         
         # condition on picture dates, to process only a few sets
         self.cond = None
+        
+        # Create temp folder
+        self.tmp_path = opj(self.project_path, "tmp")
+        if not os.path.exists(self.tmp_path): 
+            os.makedirs(self.tmp_path)
+
+        
         
     def __str__(self):
         string = "\n=======================================================================\n" \
@@ -211,17 +215,15 @@ class Photo4d(object):
         Function to initialize camera orientation of the reference set of images using the Micmac command Tapas
         '''
 
-        # add mkdir, and change dir
-        tmp_path = opj(self.project_path, "tmp")
-        if not os.path.exists(tmp_path): os.makedirs(tmp_path)
-        os.chdir(tmp_path)
-
+        # change from working dir to tmp dir
+        os.chdir(self.tmp_path)
+        
         # select the set of good pictures to estimate initial orientation
         selected_line = self.sorted_pictures[self.selected_picture_set]
         file_set = "("
         for i in range(len(self.cam_folders)):
             in_path = opj(self.cam_folders[i], selected_line[i + 1])
-            out_path = opj(tmp_path, selected_line[i + 1])
+            out_path = opj(self.tmp_path, selected_line[i + 1])
             if clahe:
                 iu.process_clahe(in_path, tileGridSize_clahe, out_path=out_path)
             else:
@@ -238,17 +240,52 @@ class Photo4d(object):
         if success == 0:
             # copy orientation file
             if os.path.exists(ori_path): rmtree(ori_path)
-            copytree(opj(tmp_path, Photo4d.ORI_FOLDER), ori_path)
+            copytree(opj(self.tmp_path, Photo4d.ORI_FOLDER), ori_path)
             self.in_ori = ori_path
         else:
             print("ERROR Orientation failed\nerror : " + str(error))
 
         os.chdir(self.project_path)
-        # todo thisraise a permission error despite this chdir I don't know why
-        try:
-            rmtree(tmp_path)
-        except PermissionError:
-            print('WARNING Cannot delete temporary folder due to permission error')
+            
+            
+    def timeSIFT_orientation(self, resolution=5000, distortion_mode='Fraser', display=True, clahe=False,
+                            tileGridSize_clahe=8):
+        '''
+        Function to initialize camera orientation of the reference set of images using the Micmac command Tapas
+        '''
+        # change from working dir to tmp dir
+        os.chdir(self.tmp_path)
+        
+        # select the set of good pictures to estimate initial orientation
+        
+        
+        for s in range(len(self.sorted_pictures)):
+            selected_line = self.sorted_pictures[s]
+            for i in range(len(self.cam_folders)):
+                in_path = opj(self.cam_folders[i], selected_line[i + 1])
+                out_path = opj(self.tmp_path, selected_line[i + 1])
+                if clahe:
+                    iu.process_clahe(in_path, tileGridSize_clahe, out_path=out_path)
+                else:
+                    copyfile(in_path, out_path)
+        file_set = ".*"+ self.ext
+
+        # Execute mm3d command for orientation
+        success, error = utils.exec_mm3d("mm3d Tapioca All {} {}".format(file_set, resolution), display=display)
+        success, error = utils.exec_mm3d(
+            "mm3d Tapas {} {} Out={}".format(distortion_mode, file_set, Photo4d.ORI_FOLDER[4:]), display=display)
+
+        ori_path = opj(self.project_path, Photo4d.ORI_FOLDER)
+        if success == 0:
+            # copy orientation file
+            if os.path.exists(ori_path): rmtree(ori_path)
+            copytree(opj(self.tmp_path, Photo4d.ORI_FOLDER), ori_path)
+            self.in_ori = ori_path
+        else:
+            print("ERROR Orientation failed\nerror : " + str(error))
+
+        os.chdir(self.project_path)
+
             
     def create_mask(self, del_pictures=True):
         '''
@@ -298,105 +335,90 @@ class Photo4d(object):
             print("ERROR prepare_GCP_files(): Check file format and file delimiter. Delimiter is any space")
             return 0
             
-    def pick_gcp_ini(self):
+    def pick_initial_gcps(self):
         '''
-        Function to initialize camera orientation
+        Function to pick GCP locations on the reference set of images with no a priori.
         
         Pick few GCPs (3 to 5) that MicMac can do a rough estimate of the camera orientation. Then go to pick_gcp_basc() to pick all GCPs of known location
-        '''
+        '''        
+        os.chdir(self.tmp_path)
+        
         if self.gcp_coord_file is None or self.gcp_names is None:
             print("ERROR prepare_gcp_files must be applied first")
         gcp_path = opj(self.project_path, Photo4d.GCP_FOLDER)
-        os.chdir(gcp_path)
-        print(gcp_path)
-        copytree(opj(self.project_path, Photo4d.ORI_FOLDER), opj(gcp_path, 'Ori-Ini'))
+        copytree(opj(gcp_path), opj(self.tmp_path))
         # select the set of image on which to pick GCPs manually
         selected_line = self.sorted_pictures[self.selected_picture_set]
         file_set = "("
         for i in range(len(self.cam_folders)):
             file_set += selected_line[i + 1] + "|"
-            in_path = opj(self.cam_folders[i], selected_line[i + 1])
-            out_path = opj(gcp_path, selected_line[i + 1])
-            copyfile(in_path, out_path)
         file_set = file_set[:-1] + ")"
             
-        print('mm3d SaisieAppuisInitQt "{}" Ori-Ini {} {}'.format(file_set, self.GCP_NAME_FILE,
-                                                                   self.GCP_PICK_FILE_INI))
-        utils.exec_mm3d(
-            'mm3d SaisieAppuisInitQt {} Ori-Ini {} {}'.format(file_set, self.GCP_NAME_FILE, self.GCP_PICK_FILE_INI))
+        commandSaisieAppuisInitQt='mm3d SaisieAppuisInitQt "{}" Ini {} {}'.format(file_set, self.GCP_NAME_FILE,
+                                                                   self.GCP_PICK_FILE)
+        print(commandSaisieAppuisInitQt)
+        utils.exec_mm3d(commandSaisieAppuisInitQt)
         
-        command = 'mm3d GCPBascule {} Ori-Ini Bascule-ini {} {}'.format(file_set,
-                                                                   self.GCP_COORD_FILE_INIT,
-                                                                   self.GCP_PICK_FILE_INI_2D)
-        print(command)
-        utils.exec_mm3d(command)
+        # Go back from tmp dir to project dir        
+        os.chdir(self.project_path)
         
-        try:
-            for image in selected_line[1:]:
-                os.remove(opj(gcp_path, image))
-            for folder in ['Ori-Ini']:
-                rmtree(folder)
-            #os.remove(Photo4d.GCP_PICK_FILE[:-4] + '-S3D.xml')
-            os.chdir(self.project_path)
-        except FileNotFoundError:
-            pass
-        except PermissionError:
-            print('WARNING Cannot delete temporary MicMac files due to permission error')
-            
-    def pick_gcp_basc(self, resolution=5000):
+        
+    def pick_all_gcps(self, resolution=5000):
         '''
-        Function to pick GCP location on the reference set of images.
+        Function to pick GCP locations on the reference set of images, with a predicted position.
         
-        Pick all GCPs of known location. Then go the pick_gcp_final() to pick extra points that will also be used as reference buut with coordinates estimated from the camera orientation.
+        Pick all GCPs of known location.
         '''
 
-        if self.gcp_coord_file is None or self.gcp_names is None:
-            print("ERROR prepare_gcp_files must be applied first")
-        gcp_path = opj(self.project_path, Photo4d.GCP_FOLDER)
-        os.chdir(gcp_path)
-        print(gcp_path)
+        os.chdir(self.tmp_path)
+
         # select the set of image on which to pick GCPs manually
         selected_line = self.sorted_pictures[self.selected_picture_set]
         file_set = "("
         for i in range(len(self.cam_folders)):
             file_set += selected_line[i + 1] + "|"
-            in_path = opj(self.cam_folders[i], selected_line[i + 1])
-            out_path = opj(gcp_path, selected_line[i + 1])
-            copyfile(in_path, out_path)
         file_set = file_set[:-1] + ")"
-            
-        command='mm3d SaisieAppuisPredicQt "{}" Ori-Bascule-ini {} {}'.format(file_set,
+        
+        print(commandBasc)
+        utils.exec_mm3d(commandBasc)
+        
+        command='mm3d SaisieAppuisPredicQt "{}" Bascule-Ini {} {}'.format(file_set,
                                                                    self.GCP_COORD_FILE_INIT,
-                                                                   self.GCP_PICK_FILE_BASC)
+                                                                   self.GCP_PICK_FILE)
         print(command)        
         utils.exec_mm3d(command)
+
+        # Go back from tmp dir to project dir        
+        os.chdir(self.project_path)
         
+    def compute_transform(self, doCampari=False):
+        '''
+        Function to apply the transformation computed from the GCPs to all images.
         
-        command="mm3d Tapioca All {} {}".format(file_set, resolution)
-        print(command)        
-        utils.exec_mm3d(command)
+        Set doCampari=True once all points are input and you are ready to carry on.
+        '''
+
+        os.chdir(self.tmp_path)
+
+        # select all the images
+        file_set = ".*" + self.ext
         
-        command = 'mm3d Campari {} Bascule-ini Bascule GCP=[{},{},{},{}] AllFree=1'.format(file_set, self.GCP_COORD_FILE_INIT, self.GCP_PRECISION, self.GCP_PICK_FILE_BASC_2D, self.GCP_POINTING_PRECISION)
-        print(command)
-        utils.exec_mm3d(command)
+        commandBasc = 'mm3d GCPBascule {} Ini Bascule-Ini {} {}'.format(file_set,
+                                                                   self.GCP_COORD_FILE_INIT,
+                                                                   self.GCP_PICK_FILE_2D)
+        print(commandBasc)
+        utils.exec_mm3d(commandBasc)
         
-        # copy orientation file
-        copytree(os.path.join(gcp_path,'Ori-Bascule'), os.path.join(self.project_path,'Ori-Bascule'))
-        self.in_ori = os.path.join(self.project_path,'Ori-Bascule')
+        if(doCampari):
+            command = 'mm3d Campari {} Bascule-Ini Bascule GCP=[{},{},{},{}] AllFree=1'.format(file_set, self.GCP_COORD_FILE_INIT, self.GCP_PRECISION, self.GCP_PICK_FILE_2D, self.GCP_POINTING_PRECISION)
+            print(command)
+            utils.exec_mm3d(command)
         
-        try:
-            for image in selected_line[1:]:
-                os.remove(opj(gcp_path, image))
-            for folder in ['Ori-Bascule_ini']:
-                rmtree(folder)
-            #os.remove(Photo4d.GCP_PICK_FILE[:-4] + '-S3D.xml')
-            os.chdir(self.project_path)
-        except FileNotFoundError:
-            pass
-        except PermissionError:
-            print('WARNING Cannot delete temporary MicMac files due to permission error')
+        # Go back from tmp dir to project dir        
+        os.chdir(self.project_path)
+
             
-    def pick_gcp_final(self):
+    def pick_ManualTiePoints(self):
         '''
         Function to pick additional points that can be set as 'GCPs'. These will get coordinates estimates based on camera orientation, and will be used in other set of images for triangulation.
         This way, we artificailly increase the number of GCPs, and use the selected set of reference images as the absolute reference to which other 3D model will be orientated against. 
@@ -404,19 +426,13 @@ class Photo4d(object):
         Pick as many points as possible that are landmarks across the all set of image.
         '''
 
-        if self.gcp_coord_file is None or self.gcp_names is None:
-            print("ERROR prepare_gcp_files must be applied first")
-        gcp_path = opj(self.project_path, Photo4d.GCP_FOLDER)
-        os.chdir(gcp_path)
-        print(gcp_path)
+        os.chdir(self.tmp_path)
+
         # select the set of image on which to pick GCPs manually
         selected_line = self.sorted_pictures[self.selected_picture_set]
         file_set = "("
         for i in range(len(self.cam_folders)):
             file_set += selected_line[i + 1] + "|"
-            in_path = opj(self.cam_folders[i], selected_line[i + 1])
-            out_path = opj(gcp_path, selected_line[i + 1])
-            copyfile(in_path, out_path)
         file_set = file_set[:-1] + ")"
             
         command='mm3d SaisieAppuisPredicQt "{}" Ori-Bascule {} {}'.format(file_set,
@@ -426,17 +442,8 @@ class Photo4d(object):
         utils.exec_mm3d(command)
         self.gcp_coord_file = opj(self.project_path,Photo4d.GCP_FOLDER, Photo4d.GCP_COORD_FILE_FINAL)
         
-        try:
-            for image in selected_line[1:]:
-                os.remove(opj(gcp_path, image))
-            for folder in ['Tmp-SaisieAppuis', 'Tmp-MM-Dir','Ori-Bascule','Homol']:
-                rmtree(folder)
-            #os.remove(Photo4d.GCP_PICK_FILE[:-4] + '-S3D.xml')
-            os.chdir(self.project_path)
-        except FileNotFoundError:
-            pass
-        except PermissionError:
-            print('WARNING Cannot delete temporary MicMac files due to permission error')
+        # Go back from tmp dir to project dir        
+        os.chdir(self.project_path)
             
     def detect_GCPs(self, kernel_size=(200, 200), display=True, save_df_gcp=True):
         
@@ -594,14 +601,39 @@ if __name__ == "__main__":
    # myproj = Photo4d(project_path=r"~/icemassME/Finse/Photo4D/2018-1pm")
    # myproj.sort_picture()
     #myproj.check_picture()
+    
     #myproj.set_selected_set("DSC02728.JPG")
-
     #myproj.initial_orientation()
     #myproj.create_mask()
     #myproj.prepare_gcp_files(r"I:\icemass-users\lucg\Finse\Photo4D\2018-1pm\GCPs_coordinates_GNSS_Only.txt",file_format="N_X_Y_Z")
-    #myproj.pick_gcp_ini()
-    #myproj.pick_gcp_basc()
+    #myproj.pick_initial_gcps()
+    #myproj.compute_transform()
+    #myproj.pick_all_gcps()
     #myproj.pick_gcp_final()
+    #myproj.compute_transform(doCampari=True)
+    
+    #myproj.timeSIFT_orientation()
+    # TODO : mask tie points
+    #myproj.prepare_gcp_files(r"C:\Users\lucg\Desktop\Test_V1_2019\GCPs_coordinates_manual.txt",file_format="N_X_Y_Z")
+    # Select a set to input GCPs
+    #myproj.set_selected_set("DSC02728.JPG")
+    # Input GCPs in 3 steps
+    #myproj.pick_initial_gcps()
+    #myproj.compute_transform()
+    #myproj.pick_all_gcps()
+    # Eventually, change selected set to add GCP imput to more image (n times):
+        #myproj.compute_transform()
+        #myproj.set_selected_set("DSC02871.JPG")
+        #myproj.pick_all_gcps()        
+    #myproj.compute_transform(doCampari=True)
+    
+    # FUNCTION TO CHANGE FOR TIMESIFT
+    # myproj.create_mask()
+    # myproj.process()
+    
+    
+    #TODO: function to clean micmac diurectory (if needed)
+    
     #myproj.detect_GCPs()
     #myproj.extract_GCPs()
     #myproj.graph_detected_gcps(50)
